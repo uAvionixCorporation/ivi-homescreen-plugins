9a698e9338411ff6a94ab3826b2ba453817013ca
Kyle Sulek <kyle.sulek@uavionix.com>
2026-05-21 15:52:10 -0500

    Added missing files
    Signed-off-by: Kyle Sulek <kyle.sulek@uavionix.com>

--- plugins/egl_image_texture/run_pigeon.sh
+++ plugins/egl_image_texture/run_pigeon.sh
@@ -1,2 +1,6 @@
 dart run pigeon \
---input pigeons/messages.dart
+--input pigeons/messages.dart \
+--dart_out lib/src/egl_image_texture_messages.g.dart \
+--cpp_header_out messages.g.h \
+--cpp_source_out messages.g.cc \
+--cpp_namespace egl_image_texture
